c867931dc38f45b200df1966dafabf01f6c8739f
Dano <mr.quadbit@gmail.com>
2021-02-04 22:21:12 -0600

    Se agreó una función que busca los midpoints en el archivo actegory actions y regresa su indice

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from random import randint
+import numpy as np
 
 F_MIDPOINTS = "Veale's script midpoints.xlsx"
+F_NOC_LIST = "Veales The NOC List.xlsx" 
+F_ACTIONS = "Veale's category actions.xlsx"
 
 def getOnestringFromStrings(strings):
 	string_list = strings.split()
@@ -33,11 +36,36 @@ def getSkeleton(df, num_tuples):
 		after_midpoint = new_tuple[2]
 	return skeleton
 
+def substituteMidPoints(midpoints, df):
+	column_when_subject = np.array(df["When Subject"])
+	positions = []
+	for tup in midpoints:
+		pos = []
+		for index, item in enumerate(column_when_subject):
+			if not isinstance(item, float):
+				if tup[0] in item:
+					pos.append(index)
+		if not pos:
+			column_when_object = np.array(df["When Object"])
+			for index, item in enumerate(column_when_object):
+				if not isinstance(item, float):
+					if tup[0] in item:
+						pos.append(index)
+			pos.append("Object")
+		else:
+			pos.append("Subject")
+		positions.append(pos)
+	return positions
+
 def main():
 	df_midpoints = pd.read_excel(F_MIDPOINTS, engine="openpyxl") #use openpyxl bc the default engine does not support xlsx formmats
-	skeleton = getSkeleton(df_midpoints, 4)
-	for tup in skeleton:
-		print(tup)
+	skeleton_midpoints = getSkeleton(df_midpoints, 4)
+
+	df_category_actions = pd.read_excel(F_ACTIONS, engine="openpyxl")
+	skeleton_characters = substituteMidPoints(skeleton_midpoints, df_category_actions)
+	for index, item in enumerate(skeleton_midpoints):
+		print(skeleton_midpoints[index], skeleton_characters[index])
+
 
 if __name__ == "__main__":
 	main()
